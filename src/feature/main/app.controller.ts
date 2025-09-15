f92b9f32c2fd45c77079f8f7f07e763358a91734
magomed <magomed.ismailov@student.efpl.be>
2025-09-15 21:05:43 +0200

    nestjs

--- src/feature/main/app.controller.ts
+++ src/feature/main/app.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {exploreGlobalApiTagsMetadata} from "@nestjs/swagger/dist/explorers/api-use-tags.explorer";
+import {AppControllerHelloWorld} from "./app.swagger";
 
 
 @ApiTags('Route de base')
 @Controller('main')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-    @ApiOperation({ summary: 'Opération HelloWorld()', description:'Cette opération est celle de base' })
+    @ApiOperation(AppControllerHelloWorld)
     @Get()
   getHello(): string {
     return this.appService.getHello();
