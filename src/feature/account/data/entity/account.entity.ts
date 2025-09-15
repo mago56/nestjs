910c4a48104884a4e887859eb80bb56132a864a6
magomed <magomed.ismailov@student.efpl.be>
2025-09-15 22:19:22 +0200

    creation des colonnes dans la DB

--- src/feature/account/data/entity/account.entity.ts
+++ src/feature/account/data/entity/account.entity.ts
@@ -1,7 +1,17 @@
+import {Column, Entity,PrimaryColumn} from 'typeorm';
+import {ulid} from 'ulid';
+
+@Entity()
 export class Account{
+    @PrimaryColumn('varchar', { length:26, default: () => `'${ulid()}'` })
     account_id:string;
+    @Column()
     firstname:string;
+    @Column()
     lastname:string;
+    @Column()
     birthdate:Date;
+    @Column()
     mail:string;
+
 }
