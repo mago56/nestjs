07290ef790a8647fda3f6a54ec85382f1ef1af05
magomed <magomed.ismailov@student.efpl.be>
2025-11-23 19:40:23 +0100

    api

--- src/feature/main/app.controller.ts
+++ src/feature/main/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {exploreGlobalApiTagsMetadata} from "@nestjs/swagger/dist/explorers/api-use-tags.explorer";
@@ -10,10 +10,23 @@ import {Public} from "@common/config";
 @Controller('main')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+
     @Public()
     @ApiOperation(AppControllerHelloWorld)
     @Get('hello-world')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Public()
+  @Get()
+    getHelloV2():string{
+      return this.appService.getHello();
+  }
+
+  @Post()
+    create(): void{
+
   }
 }
